a095c3691f51a45c8b8425efa10fabdba2224de9
hwimin-kim <gnlals7841@gmail.com>
2022-04-27 15:23:14 +0900

    chapter03 swaptest commit

---
+++ chapter03/src/main/java/chapter03/SwapTest01.java
@@ -0,0 +1,19 @@
+package chapter03;
+
+public class SwapTest01 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int temp;
+		
+		System.out.println(a + ":" + b);
+		
+		/* swap */
+		temp = a;
+		a = b;
+		b = temp;
+		
+		System.out.println(a + ":" + b);
+	}
+}
