a3afb8270ee8b87f43c8d74a46f1255b9621be20
hwimin-kim <gnlals7841@gmail.com>
2022-04-27 12:00:23 +0900

    practice03 prob01 commit

--- practice03/src/main/java/prob01/Member.java
+++ practice03/src/main/java/prob01/Member.java
@@ -1,4 +1,23 @@
 package prob01;
 
 public class Member {
+	private String id;
+	private String name;
+	private int point;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setPoint(int point) {
+		this.point = point;
+	}
+	
+	public int getPoint(int point) {
+		return this.point;
+	}
 }
