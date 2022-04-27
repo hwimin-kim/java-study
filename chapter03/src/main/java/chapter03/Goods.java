1c13e6df77b153bfbe658cb3692e5a0a3c890105
hwimin-kim <gnlals7841@gmail.com>
2022-04-27 15:22:23 +0900

    chapter03 Goods commit

--- chapter03/src/main/java/chapter03/Goods.java
+++ chapter03/src/main/java/chapter03/Goods.java
@@ -1,10 +1,15 @@
 package chapter03;
 
 public class Goods {
+	public static int countOfGoods;
 	private String name;
 	private int price;
 	private int countStock;
 	private int countSold;
+	
+	public Goods() {
+		countOfGoods += 1;
+	}	
 	
 	public String getName() {
 		return name;
@@ -33,5 +38,18 @@ public class Goods {
 	}
 	public void setCountSold(int countSold) {
 		this.countSold = countSold;
+	}
+	
+	public void showInfo() {
+		System.out.println(
+				"name: "+ name +
+				" price: "+ price +
+				" countStock: "+ countStock +
+				" countSold: "+ countSold
+				);
+	}
+	
+	public int calcDiscountPrice(double discountRate) {
+		return (int)(this.price * discountRate);
 	}
 }
--- chapter03/src/main/java/chapter03/GoodsApp.java
+++ chapter03/src/main/java/chapter03/GoodsApp.java
@@ -5,11 +5,21 @@ public class GoodsApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Goods goods = new Goods();
+		Goods goods1 = new Goods();
+		Goods goods2 = new Goods();
+		Goods goods3 = new Goods();
 		
 		goods.setName("nikon");
-		goods.setPrice(400000);
+		goods.setPrice(-1);
 		goods.setCountSold(50);
 		goods.setCountStock(30);
+		
+		System.out.println("Goods Count: "+ Goods.countOfGoods);
+		
+		goods.showInfo();
+		
+		goods.setPrice(2000);
+		int discountPrice = goods.calcDiscountPrice(0.5);
+		System.out.println("Goods Discount price: "+ discountPrice);
 	}
-
 }
