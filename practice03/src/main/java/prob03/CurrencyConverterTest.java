b277fc74a482c16c2b78a558228be866c0491014
hwimin-kim <gnlals7841@gmail.com>
2022-04-27 14:20:16 +0900

    practice03 prob03 commit

--- practice03/src/main/java/prob03/CurrencyConverterTest.java
+++ practice03/src/main/java/prob03/CurrencyConverterTest.java
@@ -3,14 +3,14 @@ package prob03;
 public class CurrencyConverterTest {
 
 	public static void main(String[] args) {
-		//  환율을 세팅 합니다.
-		//CurrencyConverter.setRate(1197.0);
+		// 22.04.27 환율
+		CurrencyConverter.setRate(1262.66);
 		
-		//double dollar = CurrencyConverter.toDollar( 1000000. );
-		//System.out.println( "백만원은 " + dollar + "달러 입니다" );
+		double dollar = CurrencyConverter.toDollar( 1000000. );
+		System.out.println( "백만원은 " + dollar + "달러 입니다" );
 		
-		//double krw = CurrencyConverter.toKRW( 100. );
-		//System.out.println( "백달러는 " + krw + "원 입니다" );
+		double krw = CurrencyConverter.toKRW( 100. );
+		System.out.println( "백달러는 " + krw + "원 입니다" );
 	}
 
 }
