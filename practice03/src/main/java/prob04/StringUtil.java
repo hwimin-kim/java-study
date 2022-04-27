ac9a1da72246812690f2a1fac1c4b5361fb63706
hwimin-kim <gnlals7841@gmail.com>
2022-04-27 14:20:34 +0900

    practice03 prob04 commit

--- practice03/src/main/java/prob04/StringUtil.java
+++ practice03/src/main/java/prob04/StringUtil.java
@@ -1,4 +1,11 @@
 package prob04;
 
 public class StringUtil {
+	public static String concatenate(String[] str) {
+		String resultStr = str[0];
+        for(int i=1; i < str.length ;i++) {
+        	resultStr = resultStr.concat(str[i]);
+        }
+        return resultStr;
+    }
 }
