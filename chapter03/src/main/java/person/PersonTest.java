408741818282055d7c0a6fc5f5bb0b3046947873
hwimin-kim <gnlals7841@gmail.com>
2022-04-28 15:20:26 +0900

    chapter03 person패키지 PersonTest 클래스 수정

--- chapter03/src/main/java/person/PersonTest.java
+++ chapter03/src/main/java/person/PersonTest.java
@@ -7,14 +7,19 @@ public class PersonTest {
 	       s1.setGrade(4);
 	       
 	       Person p1 = s1; // upcasting(암시적)
-	       //p1.getGrade();
+	       
+	       // upcasting 시 부모 클래스의 메소드A가
+	       // 자식 클래스에 오버라이딩 안되어있으면
+	       // 해당 메소드 사용 불가
+	       // p1.getGrade();
+	       
+	       // 오버라이딩된 메소드
 	       p1.getAge();
 	       
+	       // downcasting 시
+	       // 부모 클래스 메소드에 접근 가능
 	       Student s2 = (Student)p1; //downcasting(명시적)
 	       s2.getGrade();
-	       
-	       Person p2 = new Student();
-	       //p2.getGrade();
-	      	       
+      	       
 	}
 }
