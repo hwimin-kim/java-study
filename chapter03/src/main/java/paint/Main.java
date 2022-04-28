2ccd91751aedc877edb47a4da3c9c73c320615b3
hwimin-kim <gnlals7841@gmail.com>
2022-04-28 15:49:29 +0900

    instanceof 예제 추가

--- chapter03/src/main/java/paint/Main.java
+++ chapter03/src/main/java/paint/Main.java
@@ -17,25 +17,37 @@ public class Main {
 		point2.show(false);
 	
 		Point point3 = new ColorPoint(40, 50, "red");
-		//drawPoint(point3);
+		// drawPoint(point3);
 		point3.show(true);
 		point3.show(false);
 		
-		//삼각형
+		// 삼각형
 		Triangle triangle = new Triangle();
 		draw(triangle);
 		
-		//사각형
+		// 사각형
 		Rectangle rectangle = new Rectangle();
 		draw(rectangle);
 		
-		//사각형
+		// 원형
 		Circle circle = new Circle();
 		draw(circle);
 		
-		//
+		// 텍스트
 		GraphicText graphicText = new GraphicText("Helloworld");
 		draw(graphicText);
+		
+		// instanceof 연산자 테스트
+		System.out.println(circle instanceof Object);
+		System.out.println(circle instanceof Shape);
+		System.out.println(circle instanceof Circle);
+		
+		// 오류: class는 Hierachy 상위와 하위만 instaceof 연산자 사용할 수 있다.
+		// System.out.println(circle instanceof Rectangle);
+		
+		// interface는 hierachy와 상관없이 instanceof 연산자를 사용할 수 있다.
+		System.out.println(circle instanceof Drawable);
+		System.out.println(circle instanceof Runnable);
 	}
 	
 	public static void draw(Drawable drawable) {
