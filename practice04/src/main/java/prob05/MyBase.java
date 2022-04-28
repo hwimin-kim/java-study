aee38f06bf5476e803ace157e4e0b34faf334a03
hwimin-kim <gnlals7841@gmail.com>
2022-04-28 16:42:37 +0900

    practice04 prob05 commit

---
+++ practice04/src/main/java/prob05/MyBase.java
@@ -0,0 +1,22 @@
+package prob05;
+
+public class MyBase extends Base {
+	
+	public void service(String state){
+		if(state.equals("낮")) 
+			day();
+		else if(state.equals("밤"))
+			night();
+		else
+			System.out.println(state+"도 낮과 마찬가지로 일해야 합니다.");
+		
+	}
+	
+	public void day(){
+		System.out.println("낮에는 열심히 일하자!");
+	}
+	
+	public void night(){
+		System.out.println("night");
+	}
+}
