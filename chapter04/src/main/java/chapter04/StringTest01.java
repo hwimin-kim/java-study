d6a860183fb8424e4a86e42391dcde78baa36c27
hwimin-kim <gnlals7841@gmail.com>
2022-04-29 16:15:54 +0900

    String 예제1, 2

---
+++ chapter04/src/main/java/chapter04/StringTest01.java
@@ -0,0 +1,24 @@
+package chapter04;
+
+public class StringTest01 {
+
+	public static void main(String[] args) {
+		// c:\temp
+		System.out.println("c:\\temp");
+		
+		// "hello"
+		System.out.println("\"hello\"");
+		
+		// 제어문자
+		// \t: tap
+		// \r: carrige return
+		// \n: newline
+		System.out.print("hellow\tworld\n");
+		System.out.println("hellow\tworld");
+		
+		// '
+		char c = '\'';
+		System.out.println(c);
+	}
+
+}
