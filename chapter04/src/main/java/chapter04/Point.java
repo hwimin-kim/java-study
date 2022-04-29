41d0588e9b3dab8bdf192d800c92f5873f8603df
hwimin-kim <gnlals7841@gmail.com>
2022-04-29 15:19:19 +0900

    Object #finish

---
+++ chapter04/src/main/java/chapter04/Point.java
@@ -0,0 +1,19 @@
+package chapter04;
+
+import java.util.Objects;
+
+public class Point {
+	
+	private int x;
+	private int y;
+	
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public String toString() {
+		return "Point [x=" + x + ", y=" + y + "]";
+	}
+}
