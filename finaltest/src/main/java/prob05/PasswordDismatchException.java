4404af003d302b71e4ef520580f1411e588f26f1
hwimin-kim <gnlals7841@gmail.com>
2022-05-02 18:15:21 +0900

    finaltest prob5 commit

---
+++ finaltest/src/main/java/prob05/PasswordDismatchException.java
@@ -0,0 +1,8 @@
+package prob05;
+
+@SuppressWarnings("serial")
+public class PasswordDismatchException extends RuntimeException{
+	public PasswordDismatchException() {
+		super("비밀번호가 틀렸습니다.");
+	}
+}
