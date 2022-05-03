152c30e8ecf82c12d8523cf7ee1494bf7db949e9
hwimin-kim <gnlals7841@gmail.com>
2022-05-03 10:14:25 +0900

    Generic method & 가변 파라미터

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -2,19 +2,33 @@ package prob01;
 
 public class Printer {
 	
-	public void println(int data) {
-		System.out.println(data);
-	}
+	/*
+	 * public void println(int data) { System.out.println(data); }
+	 * 
+	 * public void println(boolean data) { System.out.println(data); }
+	 * 
+	 * public void println(double data) { System.out.println(data); }
+	 * 
+	 * public void println(String data) { System.out.println(data); }
+	 */
 	
-	public void println(boolean data) {
-		System.out.println(data);
-	}
+	/*
+	 * public <T> void println(T data) { System.out.println(data); }
+	 */
 	
-	public void println(double data) {
-		System.out.println(data);
+	public <T> void println(T... ts) {
+		for(T data : ts) {
+			System.out.print(data + " ");
+		}
+		System.out.println("");
 	}
-	
-	public void println(String data) {
-		System.out.println(data);
+
+	public int sum(int... nums) {
+		int sum = 0;
+		for(int i : nums) {
+			sum += i;
+		}
+		
+		return sum;
 	}
 }
--- practice04/src/main/java/prob01/PrinterTest.java
+++ practice04/src/main/java/prob01/PrinterTest.java
@@ -6,10 +6,19 @@ public class PrinterTest {
 		
 		  Printer printer = new Printer();
 		  
-		  printer.println( 10 ); 
-		  printer.println( true ); 
-		  printer.println( 5.7 );
-		  printer.println( "홍길동" );
-		 
+			/*
+			 * printer.println( 10 ); printer.println( true ); printer.println( 5.7 );
+			 * printer.println( "홍길동" );
+			 */
+		  printer.println(10);
+		  printer.println(true, 5.7);
+		  printer.println(5.7, "홍길동", 20);
+		  
+		  System.out.println(printer.sum(1, 2));
+		  System.out.println(printer.sum(1, 2, 3, 4, 5));
+		  System.out.println(printer.sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
 	}
+	
+	
+	
 }
