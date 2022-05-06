89e9a69537d259b78ee6121e51210e385e965d11
hwimin-kim <gnlals7841@gmail.com>
2022-05-06 16:23:41 +0900

    httpd #final

--- network/src/main/java/httpd/RequestHandler.java
+++ network/src/main/java/httpd/RequestHandler.java
@@ -1,9 +1,13 @@
 package httpd;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.nio.file.Files;
 
 public class RequestHandler extends Thread {
 	private Socket socket;
@@ -17,7 +21,42 @@ public class RequestHandler extends Thread {
 		try {
 			// get IOStream
 			OutputStream outputStream = socket.getOutputStream();
-
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+			
+			String request = null;
+			while(true) {
+				String line = br.readLine();
+				
+				// 브라우저가 연결을 끊으면,
+				if(line == null)
+					break;
+				
+				// SimpleHttpServer는 요청 헤더만 읽음
+				if("".equals(line))
+					break;
+				
+				//헤더의 첫 번째 라인만 읽음
+				if(request == null) {
+					request = line;
+					break;
+				}
+				
+			}
+			
+			// 요청 처리
+			//consoleLog(request);
+			String[] tokens = request.split(" ");
+			
+			if("GET".equals(tokens[0])) {
+				consoleLog("request:" + tokens[1]);
+				responseStaticResource(outputStream, tokens[1], tokens[2]);
+			} else {
+				System.out.println("!!!!!!!!!!!!!!!!!!!:" + tokens[0]);
+				// method: POST, PUT, DELETE, HEAD, CONNECT
+				// SimpleHttpServer에서는 무시(400 Bad Request 응답)
+				response400Error(outputStream, tokens[1], tokens[2]);
+			}
+					
 			// logging Remote Host IP Address & Port
 			InetSocketAddress inetSocketAddress = ( InetSocketAddress )socket.getRemoteSocketAddress();
 			consoleLog( "connected from " + inetSocketAddress.getAddress().getHostAddress() + ":" + inetSocketAddress.getPort() );
@@ -31,9 +70,7 @@ public class RequestHandler extends Thread {
 			 * "<h1>이 페이지가 잘 보이면 실습과제 SimpleHttpServer를 시작할 준비가 된 것입니다.</h1>".getBytes(
 			 * "UTF-8" ) );
 			 */
-			
-			
-			
+					
 		} catch( Exception ex ) {
 			consoleLog( "error:" + ex );
 		} finally {
@@ -49,6 +86,44 @@ public class RequestHandler extends Thread {
 		}			
 	}
 
+	private void responseStaticResource(OutputStream outputStream, String url, String protocol) throws IOException   {
+		// welcome file set
+		if("/".equals(url))
+			url = "/index.html";
+		
+		File file = new File("./webapp" + url);
+		if(file.exists())
+			response404Response(outputStream, url, protocol);
+		
+		byte[] body = Files.readAllBytes(file.toPath());
+		String contentType = Files.probeContentType(file.toPath());
+		
+		//response
+		  outputStream.write((protocol + " 200 OK\r\n").getBytes("UTF-8"));
+		  outputStream.write(("Content-Type:" + contentType + "; charset=utf-8\n").getBytes("UTF-8")); 
+		  outputStream.write("\n".getBytes());
+		  outputStream.write(body);
+		 
+	}
+	
+	private void response400Error(OutputStream outputStream, String url, String protocol) {
+		// 
+		//	HTTP/1.1 400 File Not Found\n
+		//	Content-Type: text/html; charset=utf-8\n
+		// 	\n
+		//	/errpr/400.html 내용
+		//
+	}
+
+	private void response404Response(OutputStream outputStream, String url, String protocol) {
+		// 
+		//	HTTP/1.1 404 File Not Found\n
+		//	Content-Type: text/html; charset=utf-8\n
+		// 	\n
+		//	/errpr/404.html 내용
+		//
+	}
+	
 	public void consoleLog( String message ) {
 		System.out.println( "[RequestHandler#" + getId() + "] " + message );
 	}
