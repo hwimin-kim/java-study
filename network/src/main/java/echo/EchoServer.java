ce2b53efe7a04c600b134e4b7ce4f8a5ad6b5dea
hwimin-kim <gnlals7841@gmail.com>
2022-05-06 14:14:55 +0900

    Echo client/server multiThread commit

--- network/src/main/java/echo/EchoServer.java
+++ network/src/main/java/echo/EchoServer.java
@@ -1,14 +1,10 @@
 package echo;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketException;
+
 
 public class EchoServer {
 	private static final int PORT = 6666;
@@ -22,45 +18,11 @@ public class EchoServer {
 			serverSocket.bind(new InetSocketAddress("0.0.0.0", PORT));
 			log("starts... [port:" + PORT +"]");
 			
-			
-			Socket socket = serverSocket.accept();
-			
-			InetSocketAddress inetSocketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();
-			String remoteHostAddress = inetSocketAddress.getAddress().getHostAddress();
-			int remoteHostPort = inetSocketAddress.getPort();
-			
-			log("connected by clinet[" + 
-				remoteHostAddress + ":" + 
-				remoteHostPort + "]"
-				);
-			
-			try {	
-				
-				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
-				PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"), true);
-				
-				while(true) {
-					String data = br.readLine();
-					if(data  == null) {					
-						log("closed by client");
-						break;
-					}	
-					
-					log("received:" + data);							
-					pw.println(data);				
-				}
-			} catch(SocketException ex) {
-				System.out.println("[server] suddenly close by clinet:" + ex);
-			} catch(IOException ex) {
-				System.out.println("[server] error:" + ex);
-			} finally {
-				try {
-					if(socket != null && !socket.isClosed())
-						socket.close();
-				} catch (IOException ex) {
-					ex.printStackTrace();
-				}
+			while(true) {
+				Socket socket = serverSocket.accept();
+				new EchoServerReceiveThread(socket).start();
 			}
+			
 		} catch (IOException e) {
 			System.out.println("[server] error:" + e);
 		} finally {
@@ -73,7 +35,7 @@ public class EchoServer {
 		}
 	}
 	
-	private static void log(String log) {
+	public static void log(String log) {
 		System.out.println("[Echo Server]" + log);
 	}
 
